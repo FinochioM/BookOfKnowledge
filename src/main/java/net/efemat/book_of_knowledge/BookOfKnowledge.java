66160a93b6ff24fb0cf739322ed9ffd687ae2690
FinochioM <matias.finochio@davinci.edu.ar>
2024-08-12 19:27:39 -0300

    Initial commit

---
+++ src/main/java/net/efemat/book_of_knowledge/BookOfKnowledge.java
@@ -0,0 +1,15 @@
+package net.efemat.book_of_knowledge;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class  BookOfKnowledge implements ModInitializer {
+	public static final String MOD_ID = "book_of_knowledge";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/efemat/book_of_knowledge/BookOfKnowledgeClient.java
@@ -0,0 +1,10 @@
+package net.efemat.book_of_knowledge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BookOfKnowledgeClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
